2453fb340b55718277eda04ecab8a968223b89f2
Dinesh A <56721895+dinesh-ad@users.noreply.github.com>
2021-02-17 10:13:10 +0100

    Add files via upload

---
+++ Akka/Hulk.scala
@@ -0,0 +1,42 @@
+import akka.actor._
+ 
+case object ActNormalMessage
+case object TryToFindSolution
+case object BadGuysMakeMeAngry
+ 
+class DavidBanner extends Actor{
+    import context._
+
+def angryState: Receive ={       case ActNormalMessage =>
+             println("Phew, I'm back to being David.")
+             become(normalState)
+}
+
+ def normalState: Receive ={
+        case TryToFindSolution =>
+             println("Looking for solution to my problem ...")
+        case BadGuysMakeMeAngry =>
+             println("I'm getting angry...")
+             become(angryState)
+            }
+
+ def receive ={
+        case BadGuysMakeMeAngry => become(angryState)
+        case ActNormalMessage => become(normalState)
+ }
+}
+ 
+object I_Am_Hulk extends App{
+    val system = ActorSystem("BecomeHulkExample")
+ 
+    val davidBanner = system.actorOf(Props[DavidBanner], name = "DavidBanner")
+ 
+    davidBanner ! ActNormalMessage // init to normalState
+    davidBanner ! TryToFindSolution
+    davidBanner ! BadGuysMakeMeAngry
+ 
+    Thread.sleep(1000)
+    davidBanner ! ActNormalMessage
+ 
+    //system.shutdown
+}
